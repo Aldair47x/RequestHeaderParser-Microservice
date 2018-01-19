a6a7f966e7b2d903d0d108ffef1bc3b9abd54100
Aldair47x <aldair47x@gmail.com>
2018-01-18 21:44:07 -0500

    fixing ip content

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res) {
 router.get('/api/whoami',function(req,res){
   const language = req.acceptsLanguages();
   const software = "OS: " + req.useragent.os + ", Browser: " + req.useragent.browser;
-  const ip = req.ip;
+  const ip = req.useragent.ipaddress;
   res.json({'ipaddress':ip, 'language':language[0], 'software':software}).pretty();
 });
 
