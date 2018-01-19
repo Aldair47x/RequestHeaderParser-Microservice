594c6143fe42a6778978b07e7e89c369470e1155
Aldair47x <aldair47x@gmail.com>
2018-01-18 21:21:26 -0500

    Finish the backend and frontend

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
-var users = require('./routes/users');
-
+var cors = require('cors');
+var useragent = require('express-useragent');
 var app = express();
 
 // view engine setup
@@ -17,13 +13,11 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(useragent.express());
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var useragent = require('express-useragent');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.get('/api/whoami',function(req,res){
+  const language = req.acceptsLanguages();
+  const software = "OS: " + req.useragent.os + ", Browser: " + req.useragent.browser;
+  const ip = req.ip;
+  res.json({'ipaddress':ip, 'language':language[0], 'software':software}).pretty();
 });
 
 module.exports = router;
